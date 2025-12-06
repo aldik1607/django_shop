5d039c26569a0e87ac10438da2d2e4611f1b0c68
Aldiyar <aldiyar.tandau2007@gmail.com>
2025-12-06 19:11:16 +0500

    added cart, context processors and frontend for cart

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from cart.forms import CartAddProductForm
 
 def product_list(request, category_slug=None):
     categories = Category.objects.all()
@@ -19,5 +20,8 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug = slug, available=True)
     related_products = Product.objects.filter(category = product.category,
                                               available = True).exclude(pk=product.pk)[:4]
+    cart_product_form = CartAddProductForm()
+
     return render(request, 'main/product/detail.html', {'product': product, 
-                                                         'related_products': related_products})
+                                                         'related_products': related_products,
+                                                         'cart_product_form': cart_product_form})
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'main',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -116,9 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
