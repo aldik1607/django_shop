f6ade470a929b6f90d24010ff2e6f253399facda
Aldiyar <aldiyar.tandau2007@gmail.com>
2025-12-06 14:02:49 +0500

    added  pruduct list and product detail methods in views and added paths for them, also added  frontend

--- shop/main/models.py
+++ shop/main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class  Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
@@ -11,6 +13,9 @@ class  Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('main:product_list_by_category', args=[self.slug])
     
 class Product(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -28,5 +33,8 @@ class Product(models.Model):
         
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('main:product_detail', args=[self.pk, self.slug])
 
     
